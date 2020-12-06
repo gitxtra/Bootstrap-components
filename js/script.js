b93f70247be9f245b699d7d3fca636f8051a08da
gitxtra <75396266+gitxtra@users.noreply.github.com>
2020-12-06 16:13:34 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,15 +19,20 @@
     })
 })()
 
-//   var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-//     target: '#navbar-example'
-//   });
+  var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-example'
+  });
 
-//   var dataSpyList = [].slice.call(document.querySelectorAll('[data-spy="scroll"]'))
-//   dataSpyList.forEach(function (dataSpyEl) {
-//   bootstrap.ScrollSpy.getInstance(dataSpyEl)
-//     .refresh()
-// })
+  var dataSpyList = [].slice.call(document.querySelectorAll('[data-spy="scroll"]'))
+  dataSpyList.forEach(function (dataSpyEl) {
+  bootstrap.ScrollSpy.getInstance(dataSpyEl)
+    .refresh()
+})
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
